0226e623261906cd12313089fb66728f4815e774
Guilherme Peixoto <48967037+guipeeix7@users.noreply.github.com>
2023-01-03 12:35:27 -0300

    last commit

--- teFGA.py
+++ teFGA.py
@@ -46,6 +46,14 @@ class Sat:
 
         return self.dictHW, self.dictVM
 
+    def minimizes_rule(self):
+        variable = 1
+        print("min:",end="")
+        for key, item in self.dictHW.items():
+            print(f" +1 x{str(variable)}", end = "")
+            variable += 1
+        print(";", end="")
+
     def summation_of_hardware_cpu_ram_ammount(self):
         '''
             This fuction runs steps 8 and 9 of thesis and generate rule to check if\
@@ -58,7 +66,7 @@ class Sat:
         for key, item in self.dictHW.items():
             print(f"+{item[0]} x{str(variable)}", end = " ")
             variable += 1
-        print(f">= {str(self.totalRamVM)}")
+        print(f">= {str(self.totalRamVM)};")
 
         #loop to ammount of CPU
         #Step 9
@@ -66,7 +74,7 @@ class Sat:
         for key, item in self.dictHW.items():
             print(f"+{item[1]} x{str(variable)}", end = " ")
             variable += 1
-        print(f">= {str(self.totalCpuVM)}")
+        print(f">= {str(self.totalCpuVM)};")
 
 
     def limit_hardware_provided_to_virtualmachines(self):
@@ -82,10 +90,12 @@ class Sat:
                 # item[0]
             print(f"+{itemh[0]} x{Ni}", end ="")
             Ni+=1
-            print(f" >= {self.totalRamVM}")
+            print(f" >= {self.totalRamVM};")
         
         print()
         Ni = 1
+        Ki = int(len(self.dictHW))+1
+
         for keyh, itemh in self.dictHW.items():
             for key, item in self.dictVM.items():
                 print(f"+{item[1]} ~x{str(Ki)}", end = " ")
@@ -93,13 +103,48 @@ class Sat:
                 # item[0]
             print(f"+{itemh[1]} x{Ni}", end ="")
             Ni+=1
-            print(f" >= {self.totalCpuVM}")
+            print(f" >= {self.totalCpuVM};")
+    
+    def virtual_machines_in_hardwares(self):
+        Ki = int(len(self.dictHW))+1
+        steps = Ki
+        j=0 
+        i=0
+        for keyVM, itemVM in self.dictVM.items():
+            j=0 
+
+            Ki = steps
+            for keyHW, itemHW in self.dictHW.items():
+                print(f"+{1} x{str(Ki+j+i)}", end = " ")
+                Ki+=steps
+                j+=1
+            
+            print(r">= 1;")
+            j=0 
+
+            Ki = steps
+            for keyHW, itemHW in self.dictHW.items():
+                print(f"+{1} ~x{str(Ki+j+i)}", end = " ")
+                Ki+=steps         
+                j+=1   
+            # j+=1
+            i+=1
+
+            print(f">= { steps-2 };")
+        # print(f"* #variable= {j+i} #constraint= 42")
+        # print()
 
 
 sat = Sat()
 dictHW, dictVM = sat.input()
 print(dictHW, dictVM)
+print()
+sat.minimizes_rule()
+print()
 sat.summation_of_hardware_cpu_ram_ammount()
 print()
 sat.limit_hardware_provided_to_virtualmachines()
+print()
+sat.virtual_machines_in_hardwares()
+print()
 #print(dictHW, dictVM)
