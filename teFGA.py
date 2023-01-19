e1ad425ee5b5a5892f7679c7b31dac81ef60d94f
Guilherme Peixoto <48967037+guipeeix7@users.noreply.github.com>
2023-01-19 12:58:51 -0300

    Update teFGA.py
    Interpretator implemented

--- teFGA.py
+++ teFGA.py
@@ -2,6 +2,15 @@
 # PBFVMC: A New Pseudo-Boolean Formulation to Virtual-Machine Consolidation
 # https://www.brunoribas.com.br/publicacoes/files/bracis-2013-pbfvmc.pdf
 
+from sys import stdin
+import os
+import numpy as np
+import pathlib
+
+
+pathlib.Path('./outputs').mkdir(parents=True, exist_ok=True) 
+f = open("./outputs/out.desc", "w+")
+
 class Sat:
     '''  
         dictHW {} : Dictionary containing hardware cpu and ram  
@@ -17,6 +26,8 @@ class Sat:
     totalRamHW = 0
     totalCpuHW = 0
     variables = 0
+    n = 0 
+    m = 0
 
     def __construct():
         return
@@ -27,6 +38,7 @@ class Sat:
             dictHW, dictVM, totalRamVM, totalCpuVM variables values
         '''
         n = int(input())
+        self.n = n
         for i in range(n):
             nomeH, ramH, cpuH = input().split()
             self.dictHW[nomeH] = [ramH, cpuH]
@@ -35,6 +47,7 @@ class Sat:
             self.totalCpuHW += int(cpuH)
 
         m = int(input())
+        self.m = m
         for j in range(m):
             nomeV, ramV, cpuV = input().split()
             self.dictVM[nomeV] = [ramV, cpuV]
@@ -46,13 +59,62 @@ class Sat:
 
         return self.dictHW, self.dictVM
 
+    def claspHandler(self):
+        linearArray = []
+
+        with open('./outputs/outClasp.txt') as f:
+            lines = f.readlines()
+            for line in lines:
+                if(line[0] == 'v'):
+                    tempLine = line[1:]
+                    tempLineArr = tempLine.split()
+                    linearArray = np.concatenate((linearArray, tempLineArr), axis=None)
+        
+        self.getMachines(linearArray)
+        self.getVirtuals(linearArray)
+        return linearArray     
+                    
+    def getVirtuals(self, linearArray):
+        matrix = linearArray[self.n :].reshape(self.n,self.m)
+        
+        
+        print("VM" , end="    ")
+        for i in range(0, (self.m)):
+            print(f"{i+1}", end=" ")
+        print()
+        
+        for i in range(0, (self.n)):
+            print(f"HW({i+1})", end = " ")
+            for j in range(0, self.m):
+                if("-" in matrix[i, j]):
+                    print(0, end=" ")
+                else:
+                    print(1, end = " ")
+                # print(f"HW {i+1} {matrix[i, j]}")
+            print()
+
+
+    def getMachines(self, tempLineArr):
+        HwMachines = []
+        i = 0 
+        for i in range(1, self.n):
+            if('-' not in tempLineArr[i]):
+                HwMachines.append(1)
+            else:
+                HwMachines.append(0)
+        # print(HwMachines)
+        return (i)
+    
+    
+    
+    
     def minimizes_rule(self):
         variable = 1
-        print("min:",end="")
+        f.write("min:")
         for key, item in self.dictHW.items():
-            print(f" +1 x{str(variable)}", end = "")
+            f.write(f" +1 x{str(variable)}")
             variable += 1
-        print(";", end="")
+        f.write(";")
 
     def summation_of_hardware_cpu_ram_ammount(self):
         '''
@@ -64,17 +126,17 @@ class Sat:
         #Step 8
         variable = 1
         for key, item in self.dictHW.items():
-            print(f"+{item[0]} x{str(variable)}", end = " ")
+            f.write(f"+{item[0]} x{str(variable)} ")
             variable += 1
-        print(f">= {str(self.totalRamVM)};")
+        f.write(f">= {str(self.totalRamVM)};")
         
         #loop to ammount of CPU
         #Step 9
         variable = 1
         for key, item in self.dictHW.items():
-            print(f"+{item[1]} x{str(variable)}", end = " ")
+            f.write(f"+{item[1]} x{str(variable)} ")
             variable += 1
-        print(f">= {str(self.totalCpuVM)};")
+        f.write(f">= {str(self.totalCpuVM)};")
 
 
     def limit_hardware_provided_to_virtualmachines(self):
@@ -85,24 +147,24 @@ class Sat:
         Ni = 1
         for keyh, itemh in self.dictHW.items():
             for key, item in self.dictVM.items():
-                print(f"+{item[0]} ~x{str(Ki)}", end = " ")
+                f.write(f"+{item[0]} ~x{str(Ki)} ")
                 Ki+=1
                 # item[0]
-            print(f"+{itemh[0]} x{Ni}", end ="")
+            f.write(f"+{itemh[0]} x{Ni}")
             Ni+=1
-            print(f" >= {self.totalRamVM};")
+            f.write(f" >= {self.totalRamVM};")
         
         Ni = 1
         Ki = int(len(self.dictHW))+1
 
         for keyh, itemh in self.dictHW.items():
             for key, item in self.dictVM.items():
-                print(f"+{item[1]} ~x{str(Ki)}", end = " ")
+                f.write(f"+{item[1]} ~x{str(Ki)} ")
                 Ki+=1
                 # item[0]
-            print(f"+{itemh[1]} x{Ni}", end ="")
+            f.write(f"+{itemh[1]} x{Ni}")
             Ni+=1
-            print(f" >= {self.totalCpuVM};")
+            f.write(f" >= {self.totalCpuVM};")
     
     def virtual_machines_in_hardwares(self):
         totalHardwares = int(len(self.dictHW))
@@ -114,22 +176,22 @@ class Sat:
         for keyVM, itemVM in self.dictVM.items():
             kj=0
             for keyHW, itemHW in self.dictHW.items():
-                print(f"+{1} x{str(kj+j+totalHardwares)}", end = " ")
+                f.write(f"+{1} x{str(kj+j+totalHardwares)} ")
                 kj+=totalVms            
-            print(r">= 1;")
+            f.write(r">= 1;")
 
             kj=0
             for keyHW, itemHW in self.dictHW.items():
-                print(f"+{1} ~x{str(kj+j+totalHardwares)}", end = " ")
+                f.write(f"+{1} ~x{str(kj+j+totalHardwares)} ")
                 kj+=totalVms         
             j+=1
 
-            print(f">= { totalHardwares-1 };")
+            f.write(f">= { totalHardwares-1 };")
+
 
 
 sat = Sat()
 dictHW, dictVM = sat.input()
-# print(dictHW, dictVM)
 #Total number of Hardwares * Virtual Machines + Number of hardwares
 # In another word is the number of variables used in Virtual machines +\
 # The possibility that each virtual machine cold be allocated in each hardware
@@ -144,15 +206,28 @@ variables = (totalHW * totalVM) + totalHW
 #2*(number of VM's) constrains generated in step 12 and 13 
 constraint= (2 + 2*totalHW + 2*totalVM)
 
-print(f"* #variable= {variables} #constraint= {constraint}")
-# print()
+f.write(f"* #variable= {variables} #constraint= {constraint}")
+
 sat.minimizes_rule()
-# print()
 sat.summation_of_hardware_cpu_ram_ammount()
-# print()
 sat.limit_hardware_provided_to_virtualmachines()
-# print()
 sat.virtual_machines_in_hardwares()
-# print()
-#print(dictHW, dictVM)
-print(f"* #variable= {variables} #constraint= {constraint}")
+
+f.write(f"* #variable= {variables} #constraint= {constraint}")
+
+
+f.close()
+os.system('clasp ./outputs/out.desc > ./outputs/outClasp.txt')
+
+sat.claspHandler()
+
+
+if os.path.exists("./outputs/out.desc"):
+  os.remove("./outputs/out.desc")
+else:
+  print("The file does not exist")
+  
+if os.path.exists("./outputs/outClasp.txt"):
+  os.remove("./outputs/outClasp.txt")
+else:
+  print("The file does not exist")
